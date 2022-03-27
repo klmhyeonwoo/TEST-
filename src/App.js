da86ef0b2dd09c88c8b1e5e0ec768e301a73a41c
klmhyeonwooo <gentlemonster77@naver.com>
2022-03-27 23:01:12 +0900

    github test commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 import './App.css';
 import {Title, Subject} from './hello';
 import logo from './img/mImg.png';
+import { useState } from 'react';
 
 function App() {
+
   return (
     <div className = "text">
+      <div> 값 : {number} </div>
       <Title />
-      <img className = "img" src={logo} alt="aa"/>
+      <img className = "img"  src={logo} alt="aa"/>
       <Subject />
     </div>
   );
